8e8b2bbfaa47c15a63e165b1b557860ff7ea93f3
gousemohiddin44 <78602748+gousemohiddin44@users.noreply.github.com>
2021-07-18 13:00:03 +0530

    All files

---
+++ app.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const ejs = require("ejs");
+//const bcrypt  = require("bcryptjs");
+const config = require("config");
+//const jwt = require("jsonwebtoken");
+const posts = require("./posts");
+const connectDB = require("./config/db");
+const User = require("./model/user.js");
+const user = require("./model/user.js");
+
+const app =express();
+
+app.set("view engine","ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+connectDB();
+
+app.get("/topic/:title",(req,res)=>{
+    posts.forEach((post)=>{
+        if(req.params.title===post.title){
+            res.render("topic",{post:post});
+        }
+    });
+});
+
+app.get("/",(req,res)=>{
+    res.render("Login");
+});
+
+
+
+app.post("/",(req,res)=>{
+    //verify login here
+
+    const {username,password}=req.body;
+
+    User.findOne({email:username},(err,founduser)=>{
+        if(err) {alert("User not found")}
+        else{
+            if(founduser.password === password){
+                res.redirect("/blog");
+            }
+            else{
+                res.redirect("/");
+            }
+        }
+    })
+
+});
+
+app.get("/signin",(req,res)=>{
+    res.render("signin");
+});
+
+app.post("/signin",(req,res)=>{
+    //save new user details
+
+    const {email,password}=req.body;
+
+    const newuser = new User({
+        email:email,
+        password:password
+    });
+
+    // const salt = bcrypt.genSalt(10);
+
+    // newuser.password = bcrypt.hash(password,salt);
+
+    newuser.save();
+
+    const payload={
+        user:{
+            id:user.id
+        }
+    };
+
+    //  jwt.sign(payload, config.get("jwtsecret"),(err,token)=>{
+    //     if(err) throw err;
+    //     res.json({token});
+    // });
+
+
+
+    res.redirect("/");
+});
+
+
+app.get("/blog",(req,res)=>{
+    res.render("blog",{post:posts});
+});
+
+app.post("/blog",(req,res)=>{
+    const {imgurl,title,content} = req.body;
+    const blogobj={
+        imgurl:imgurl,
+        title:title,
+        content:content
+    };
+    posts.push(blogobj);
+    res.redirect("/blog");
+})
+
+app.get("/createBlog",(req,res)=>{
+    res.render("createBlog");
+});
+
+app.get("/delete/:topic",(req,res)=>{
+    posts.forEach((post)=>{
+        if(req.params.topic===post.title){
+            const index = posts.indexOf(post.title);
+            if(index >= -1) posts.splice(index,1);
+        }
+    });
+    res.redirect("/blog");
+
+});
+
+app.get("/editBlog/:topic",(req,res)=>{
+    posts.forEach((post)=>{
+        if(req.params.topic===post.title){
+            res.render("editBlog",{post:post});
+        }
+    })
+    
+});
+
+app.post("/editBlog/:topic",(req,res)=>{
+    posts.forEach((post)=>{
+        if(req.params.topic===post.title){
+            const index = posts.indexOf(post.title);
+                console.log(posts[index]);
+        }
+    })
+    res.redirect("/blog");
+});
+
+
+app.get("/home",(req,res)=>{
+    res.render("/")
+});
+
+app.get("/about",(req,res)=>{
+    res.render("about")
+});
+
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+});
+
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Server running ...");
+})
---
+++ posts.js
@@ -0,0 +1,24 @@
+const posts =[
+    {
+        imgurl:"https://easybase.io/assets/images/posts_images/5-great-react-libraries-1.png",
+        title:"React",
+        content:"This is a module for beginners planning to learn React. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. "
+    },
+    {
+        imgurl:"https://cdn.pixabay.com/photo/2017/06/20/23/15/coffee-2425303__340.jpg",
+        title:"Express",
+        content:"This is a module for beginners planning to learn Express .Contrary to popular belief,Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
+    },
+    {
+        imgurl:"https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399__480.png",
+        title:"Node",
+        content:"This is a module for beginners planning to learn Node. Contrary to popular belief,comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a  Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
+    },
+    {
+        imgurl:"",
+        title:"Mongo",
+        content:"This is a module for beginners planning to learn Mongo.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a discovered the undoubtable source."
+    }
+];
+
+module.exports = posts;
